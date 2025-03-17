35f53a133b21ef30e0a3e04c2f7e5e9ff09546ec
Christiannnnn1 <christianallenreonal@thelewiscollege.edu.ph>
2025-03-17 18:48:27 +0800

    Updated booking.jsx file

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -104,6 +104,7 @@ const HotelBookingSystem = () => {
           </button>
         </div>
       </div>
+      
       <div className="max-w-6xl mx-auto p-4 font-sans">  
         <form onSubmit={handleSubmit}>
           <div className="flex flex-col md:flex-row gap-6">
